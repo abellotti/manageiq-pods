60cfdc5da3f4a93e0d1f6c1e5aeffe828f95f9b8
Alberto Bellotti <abellott@redhat.com>
2017-06-01 14:30:04 -0400

    Enhancements to miq_template.yaml for supporting a seperate Apache container

--- images/miq-app/docker-assets/appliance-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[[ -s /container.env.vars ]] && source /container.env.vars
-
-appliance-initialize.sh
-
-[[ -s /etc/default/evm ]] && source /etc/default/evm
-
-ruby /var/www/miq/vmdb/lib/workers/bin/evm_server.rb
